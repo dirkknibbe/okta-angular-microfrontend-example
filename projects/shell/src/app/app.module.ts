7043ac6019d7cddabda7501472f5f9c6937f0ca3
dirkknibbe <dirk.knibbe@yahoo.com>
2023-02-03 08:59:33 -0600

    put in the signIn signOut click handlers in the HTML

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductComponent } from './products/product.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductsComponent } from "./products/products.component";
+import { ProductComponent } from "./products/product.component";
+
+import { OKTA_CONFIG, OktaAuthModule } from "@okta/okta-angular";
+import { OktaAuth } from "@okta/okta-auth-js";
+
+const oktaAuth = new OktaAuth({
+  issuer: "https://dev-90421809.okta.com/oauth2/default",
+  clientId: "0oa8731fmdUQktuvk5d7",
+  redirectUri: window.location.origin + "/login/callback",
+  scopes: ["openid", "profile", "email"],
+});
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductsComponent, ProductComponent],
+  imports: [BrowserModule, AppRoutingModule, OktaAuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
